0b05680b9a3ae71cecb45206073c5aa6449ca97f
TMcMurtry <mr.timmcmurtry@gmail.com>
2026-09-09 11:14:33 -0500

    added table annotation to model classes

--- remember-this-day-backend/src/main/java/models/Entries.java
+++ remember-this-day-backend/src/main/java/models/Entries.java
@@ -1,11 +1,9 @@
 package models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name= "entries")
 public class Entries {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- remember-this-day-backend/src/main/java/models/Prompts.java
+++ remember-this-day-backend/src/main/java/models/Prompts.java
@@ -1,11 +1,9 @@
 package models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "prompts")
 public class Prompts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
